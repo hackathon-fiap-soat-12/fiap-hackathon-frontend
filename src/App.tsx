9a1d2d7d46125ad910ab0c503be5e7668f69a7aa
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-03-12 23:39:33 -0300

    feat: :construction: Added toast component with example

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import { useState } from 'react';
 import { Button } from '@/core/components/ui/button';
 import './App.css';
+import { ModeToggle } from '@/core/components/mode-toggle';
+import { toast } from 'sonner';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="card">
-        <div className="flex flex-col items-center justify-center min-h-svh">
-          <Button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </Button>
-        </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <ModeToggle />
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button
+          onClick={() => {
+            setCount((count) => count + 1);
+            toast('Event has been created', {
+              description: 'Sunday, December 03, 2023 at 9:00 AM',
+              action: {
+                label: 'Undo',
+                onClick: () => console.log('Undo'),
+              },
+            });
+          }}
+        >
+          count is {count}
+        </Button>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { ThemeProvider } from '@/core/components/theme-provider';
+import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+      <Toaster />
+    </ThemeProvider>
+  </StrictMode>
+);
