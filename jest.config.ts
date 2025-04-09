e013690de012b4054f7aefb88dd052b99d01bda0
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-04-08 22:52:42 -0300

    feat: :construction: Add configs to jest

---
+++ jest.config.ts
@@ -0,0 +1,50 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  // Padrão de busca global para todos os arquivos de teste
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js|jsx)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js|jsx)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+
+  moduleNameMapper: {
+    '^@/shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@/modules/(.*)$': '<rootDir>/src/modules/$1',
+    '^@/core/(.*)$': '<rootDir>/src/core/$1',
+    '^@/app/(.*)$': '<rootDir>/src/app/$1',
+    '^@/assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@/i18n/(.*)$': '<rootDir>/src/infra/i18n/$1',
+  },
+
+  // Configurações de coverage
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.tsx',
+    '!src/**/index.ts',
+    '!src/main.tsx',
+    '!src/**/__mocks__/**',
+    '!src/**/__tests__/**',
+  ],
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      branches: 70,
+      functions: 70,
+      lines: 70,
+    },
+  },
+};
+
+export default config;
