4326a1237d09f641adb976d6c1f7a6a021f5ebaa
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-04-18 23:58:47 -0300

    feat: Update component to add progressbar with lazy progress
    Add outputs to manage component and handle upload

--- src/shared/components/file-uploader/file-uploader.tsx
+++ src/shared/components/file-uploader/file-uploader.tsx
@@ -2,28 +2,52 @@ import { Button } from '@/core/components/ui/button';
 import { Input } from '@/core/components/ui/input';
 import { Progress } from '@/core/components/ui/progress';
 import { cn } from '@/core/lib/utils';
+import { validateMp4File } from '@/shared/utils/file-validations.utils';
 import { Upload, X } from 'lucide-react';
 import type React from 'react';
 import { useRef, useState } from 'react';
+import { toast } from 'sonner';
 
 interface FileUploaderProps {
   onFilesChange?: (files: File[]) => void;
+  progress?: number;
+  isUploading?: boolean;
+  onUploadClick?: () => void;
+  onCancel?: () => void;
 }
 
-export function FileUploader({ onFilesChange }: FileUploaderProps) {
+export function FileUploader({
+  onFilesChange,
+  progress = 0,
+  isUploading = false,
+  onUploadClick,
+  onCancel,
+}: FileUploaderProps) {
   const [files, setFiles] = useState<File[]>([]);
-  const [progress, setProgress] = useState(0);
-  const [isUploading, setIsUploading] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      setFiles(Array.from(e.target.files));
+    if (!e.target?.files?.length || !validateMp4File(e.target.files[0])) {
+      toast.error('Oops, não será feito o upload!', {
+        description:
+          'O arquivo deve ser um vídeo MP4 com tamanho máximo de 500 MB',
+      });
+      removeFile();
+      return;
     }
+
+    setFiles(Array.from(e.target.files));
 
     if (onFilesChange) {
       onFilesChange(e.target.files ? Array.from(e.target.files) : []);
     }
+  };
+
+  const onCancelFile = () => {
+    if (onCancel) {
+      onCancel();
+    }
+    removeFile();
   };
 
   const removeFile = () => {
@@ -33,22 +57,10 @@ export function FileUploader({ onFilesChange }: FileUploaderProps) {
     }
   };
 
-  const simulateUpload = () => {
-    if (files.length === 0) return;
-
-    setIsUploading(true);
-    setProgress(0);
-
-    const interval = setInterval(() => {
-      setProgress((prevProgress) => {
-        if (prevProgress >= 100) {
-          clearInterval(interval);
-          setIsUploading(false);
-          return 100;
-        }
-        return prevProgress + 5;
-      });
-    }, 200);
+  const handleUploadClick = () => {
+    if (onUploadClick) {
+      onUploadClick();
+    }
   };
 
   return (
@@ -69,8 +81,8 @@ export function FileUploader({ onFilesChange }: FileUploaderProps) {
             <Button
               variant="outline"
               size="icon"
-              onClick={removeFile}
-              disabled={isUploading}
+              onClick={onCancelFile}
+              disabled={!isUploading}
               className="border-gray-700 bg-gray-800 text-gray-200 hover:bg-gray-700 hover:text-white"
             >
               <X className="h-4 w-4" />
@@ -100,8 +112,8 @@ export function FileUploader({ onFilesChange }: FileUploaderProps) {
       )}
 
       <Button
-        onClick={simulateUpload}
-        disabled={files.length === 0 || isUploading}
+        onClick={handleUploadClick}
+        disabled={files.length === 0 || isUploading || progress === 100}
         className="w-full bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:ring-indigo-500 disabled:bg-indigo-600/50"
       >
         <Upload className="mr-2 h-4 w-4" />
